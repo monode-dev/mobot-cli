3d9b0065f135d37d63e587b7869dea83bee32d5e
MechMel <15083231+MechMel@users.noreply.github.com>
2024-06-22 12:19:28 -0700

    Publishing 0.0.27

--- src/mobot-cli.ts
+++ src/mobot-cli.ts
@@ -5,6 +5,8 @@ import fs from "fs";
 import path from "path";
 import { execSync } from "child_process";
 import packageJson from "../package.json";
+import { createServer } from "vite";
+import puppeteer from "puppeteer";
 
 // Version & Program description
 program
@@ -16,7 +18,25 @@ program
   .command(`sync`)
   .description(`Syncs the current project to the .mobot_cache directory`)
   .action(async function () {
-    console.log(`"sync" is not yet implemented`);
+    const server = await createServer();
+    await server.listen();
+    const port = server.config.server.port;
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    const url = `http://localhost:${port}`;
+    console.log("Opening the generated website:", url);
+    await page.goto(url, {
+      waitUntil: "networkidle0",
+    });
+    // Extract the configuration from the page
+    const iacConfig = await page.evaluate(`window.iacConfig`);
+
+    console.log("IAC Configuration:", iacConfig);
+
+    // Process the configuration as needed
+    // For example, deploy infrastructure, validate configuration, etc.
+
+    await browser.close();
   });
 
 // Sync the current project to GitHub
